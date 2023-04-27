5172f85179db816c89543b8ad670999e42e2d5e5
Gxgxn <gdsinghb99@gmail.com>
2023-04-27 19:17:05 -0300

    added api test

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 if (process.argv.length < 3) {
   console.log("give password as argument");
   process.exit(1);
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://gxgxn:${password}@cluster1.baqh6ry.mongodb.net/testNotesApp`;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
@@ -24,22 +24,20 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema);
 
-// const note = new Note({
-//   content: "Mongoose makes things easy",
-//   date: new Date(),
-//   important: true,
-// });
+const note = new Note({
+  content: "Browser can execute only JavaScript",
+  date: new Date(),
+  important: true,
+});
 
-/*
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
-*/
-
-Note.find({}).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
+note.save().then((result) => {
+  console.log("note saved!");
   mongoose.connection.close();
 });
+
+// Note.find({}).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
